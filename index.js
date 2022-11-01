6c47d53762e1f6b8f9c9f5eacc464189d35a23f6
oliiversion <olivxrkei101@gmail.com>
2022-11-01 13:15:21 -0400

    ye

--- index.js
+++ index.js
@@ -1,43 +1,52 @@
 const fruityflags = [
-    {
-      name: "Gay",
-      colors: "blue+green+white",
-      meaning: "non-women+non-women",
-      fruitylevel: "100%"
-    },
-    {
-      name: "Lesbian",
-      colors: "pink+orange+white",
-      meaning: "nonmen+nonmen",
-      fruitylevel: "100%"
-    },
-    {
-      name: "Bisexual",
-      colors: "blue+purple+pink",
-      meaning: "anyone+other/samesex",
-      fruitylevel: "50%"
-    },
-    {
-      name: "Pansexual",
-      colors: "yellow+pink+blue",
-      meaning: "anyone+anyone",
-      fruitylevel: "65%"
-    },
-    {
-      name: "Asexual",
-      colors: "purple+gray+white+black",
-      meaning: "L+sex.",
-      fruitylevel: "125%"
-    },
-  ];
-  fruityflags.forEach((fruityflags) => console.log(fruityflags.name));
+  {
+    name: "Gay",
+    colors: ["blue", "green", "white"],
+    meaning: "non-women+non-women",
+    fruitylevel: 100,
+  },
+  {
+    name: "Lesbian",
+    colors: ["pink", "orange", "white"],
+    meaning: "nonmen+nonmen",
+    fruitylevel: 100,
+  },
+  {
+    name: "Bisexual",
+    colors: ["blue", "purple", "pink"],
+    meaning: "anyone+other/samesex",
+    fruitylevel: 50,
+  },
+  {
+    name: "Pansexual",
+    colors: ["yellow", "pink", "blue"],
+    meaning: "anyone+anyone",
+    fruitylevel: 65,
+  },
+  {
+    name: "Asexual",
+    colors: ["purple", "gray", "white", "black"],
+    meaning: "L+sex.",
+    fruitylevel: 125,
+  },
+];
+/* fruityflags.forEach((fruityflags) => console.log(fruityflags.name));
 
-  fruityflags.forEach((fruityflags) => {
-    fruityflags.fruitylevel.forEach(fruityflags.fruitylevel) > console.log(fruityflags.fruitylevel);
+fruityflags.forEach((fruityflags) => {
+  fruityflags.fruitylevel.forEach(fruityflags.fruitylevel) >
+    console.log(fruityflags.fruitylevel);
+});
+
+console.log(fruityflags);
+
+const result = fruityflags.filter(
+  (fruityflags) => (fruityflags.colors = "blue+green+white")
+);
+console.log(result);
+ */
+
+fruityflags
+  .filter((fruityflags) => fruityflags.fruitylevel > 50)
+  .forEach((el) => {
+    console.log(el.name, el.colors);
   });
-
-  console.log(fruityflags);
-
-  const result = fruityflags.filter((fruityflags) => (fruityflags.colors = "blue+green+white"));
-  console.log(result);
-  
